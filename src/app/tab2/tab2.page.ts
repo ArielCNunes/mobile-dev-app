1605a9576cc2b9e0e670a98c2b19ca21a628a9bd
Ariel Nunes <g00418763@atu.ie>
2024-04-18 14:03:05 +0100

    adding two-way data binding (ngModel)

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -4,21 +4,23 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
 
 import { MoviesInfoService } from '../Services/movies-info.service';
 import { CommonModule } from '@angular/common'; // for *ngFor
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule, IonCard, IonCardHeader, IonImg, IonItem, IonInput, IonButton, IonIcon]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent, CommonModule, IonCard, IonCardHeader, IonImg, IonItem, IonInput, IonButton, IonIcon, FormsModule]
 })
 export class Tab2Page implements OnInit {
-  Movies:any = [];
+  Movies:any = []; // array holding all movies
+  searchTerm: string = ''; // movie title
 
   constructor(private movieImagesService:MoviesInfoService) { }
 
   ngOnInit(): void {
-      // Default search
+      // Default search - this is what the user will see the page is first launched
       this.getMovie("action");
   }
 
