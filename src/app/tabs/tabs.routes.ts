d1439e182c9202eb0756adc4a1f9759ec7fb11d6
Ariel Nunes <g00418763@atu.ie>
2024-04-18 14:24:55 +0100

    removed tab 1 (trailers) form the app and updated routes and icons

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,11 +7,6 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
         path: 'tab2',
         loadComponent: () =>
           import('../tab2/tab2.page').then((m) => m.Tab2Page),
