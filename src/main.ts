236a54326ce2d3dbd98a5d939186616dfb211f1e
Ariel Nunes <g00418763@atu.ie>
2024-05-01 21:46:04 +0100

    minor adjustments. Still trying to get the posters to stay in the watchlist after the page has been reloaded (data persitence)

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ import { environment } from './environments/environment';
 // 1
 import { HttpClientModule } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 if (environment.production) {
   enableProdMode();
@@ -20,6 +21,7 @@ bootstrapApplication(AppComponent, {
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes),
+    importProvidersFrom(IonicStorageModule.forRoot()),
 
     // 2
     importProvidersFrom(HttpClientModule)
